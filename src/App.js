2781ffd39c6e5da3afd7e182361e2e3f3d891931
LeoImenes <leonardoimenes@hotmail.com>
2022-01-10 17:33:10 -0300

    testes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import "./App.css";
-import Evento from './components/Evento'
-import Form from './components/Form'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const nome = "Jose"
-return (
+  return (
     <div className="App">
-      <h1>Ola mundo</h1>
-      <Evento/>
-      <Form/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
